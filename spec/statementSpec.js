d3bb05e0c5f25860f6a8db2d0feee8783ac0039e
Riya Dattani <riya_dattani@hotmail.com>
2019-05-08 10:54:44 +0100

    refactored StatementSpec

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -2,10 +2,13 @@ describe("Statement", function() {
   var account = new Account;
 
   it("should print the transactions made in the account", function () {
-    account.deposit(80)
-    account.withdraw(50)
+    var oldDate = new Date(2019, 5, 1);
+    stubDate = account.getFormattedDate(oldDate);
+
+    account.deposit(80, stubDate);
+    account.withdraw(50, stubDate)
     statement = new Statement(account)
-    expect(statement.print()).toEqual("date || credit || debit || balance \n7/5/2019 || || 80 || 80 \n7/5/2019 || 50 || || 30 \n")
+    expect(statement.print()).toEqual("date || credit || debit || balance \n1/6/2019 || || 80 || 80 \n1/6/2019 || 50 || || 30 \n")
   });
 
 });
